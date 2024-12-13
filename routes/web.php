a585dadd0fd3958fcb9c282c56a8188fc29cfd5c
Intan <nurintanqolbinuri23@politala.ac.id>
2024-12-13 15:30:52 +0800

    rute

--- routes/web.php
+++ routes/web.php
@@ -47,3 +47,13 @@ Route::middleware([RoleMiddleware:: class . ':user'])->group(function () {
     Route::delete('/profil/{id}', [ProfilController::class, 'destroy'])->name('profil.destroy');
     Route::get('/profil/{id}/edit', [ProfilController::class, 'edit'])->name('profil.edit');
 });
+
+Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
+Route::post('/cart/add', [CartController::class, 'add'])->name('cart.add');
+Route::post('/cart/remove', [CartController::class, 'remove'])->name('cart.remove');
+Route::post('/cart/checkout', [CartController::class, 'checkout'])->name('cart.checkout');
+Route::get('/cart/update', [CartController::class, 'update'])->name('cart.update');
+
+Route::post('/checkout', [TransactionController::class, 'checkout'])->name('cart.checkout');
+Route::get('/transactions', [TransactionController::class, 'index'])->name('transactions.index');
+
